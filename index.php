8e4ae0e34a91490ea30108b93407c894f8e1245d
Jeppe Ernst <jeppe@colourbox.com>
2017-12-15 10:25:23 +0100

    springle some color on it

--- index.php
+++ index.php
@@ -1,10 +1,48 @@
-Host:
-<?php 
-echo gethostname();
-//phpinfo();
-?>
-<br>
-Ansible was last executed by:
-<?php
-echo getenv("ansibleExecuter");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body.machine1{
+            background: #11998e;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #38ef7d, #11998e);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #38ef7d, #11998e); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+        }
+        body.machine2{
+            background: #fc4a1a;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #f7b733, #fc4a1a);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #f7b733, #fc4a1a); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */            
+        }
+        body{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            min-height: 100%;
+        }
+        div{
+            text-align: center;
+            font-size: 2rem;
+            color: #FFF;
+            text-transform: capitalize;
+        }
+    </style>
+</head>
+<body class="<?php echo gethostname(); ?>">
+    <div>
+    Host:
+    <?php 
+    echo gethostname();
+    //phpinfo();
+    ?>
+    <br>
+    Ansible was last executed by:
+    <?php
+    echo getenv("ansibleExecuter");
+    ?>
+    </div>
+</body>
+</html>
